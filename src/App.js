1dd3c311e3a7bc4cbca4d7dea18179be585497cf
Ezeoke Stanley Ifeanyi <ezeokestanley.ifeanyi@gmail.com>
2023-02-06 09:43:28 +0100

    Add navigation drawer MUI practical

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+
+    </div>
+  );
+ 
+}
+
+export default App;
