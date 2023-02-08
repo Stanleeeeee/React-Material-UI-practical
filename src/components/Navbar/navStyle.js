910072d6f2515996ea946a6bea7d967a2e9ce16c
Ezeoke Stanley Ifeanyi <ezeokestanley.ifeanyi@gmail.com>
2023-02-08 16:07:24 +0100

    Add usenavigate and onClick method

--- src/components/Navbar/navStyle.js
+++ src/components/Navbar/navStyle.js
@@ -11,5 +11,16 @@ export const NavbarStyle = {
         '& .Mui-selected': {
             color: 'red',
         },
+    },
+    icons: {
+        color: 'rgba(255, 255, 255, 0.7)!important',
+        marginLeft: '20px',
+    },
+    text: {
+        '& span': {
+            marginLeft: '-10px',
+            fontWeight: '600',
+            fontSize: '16px',
+        }
     }
 };
