56561ad35545bd24001111863fb150128d6711cb
Ezeoke Stanley Ifeanyi <ezeokestanley.ifeanyi@gmail.com>
2023-02-08 16:31:48 +0100

    Add app routes for the dashboard

--- src/index.js
+++ src/index.js
@@ -1,30 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-/*import {
+import {
   BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
+import Authentication from './pages/Authentication/Authentication';
+import MachineLearning from './pages/MachineLearning/MachineLearning';
+import Hosting from './pages/Hosting/Hosting';
+import Functions from './pages/Functions/Functions';
+import Database from './pages/Database/Database';
+import Storage from './pages/Storage/Storage';
 
 ReactDOM.render(
-  <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />}>
-      <Route path="authentication" element={< />} />
-    </Route>
-  </Routes>
-</BrowserRouter>,
-); */
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="authentication" element={<Authentication />} />
+        <Route path="database" element={<Database />} />
+        <Route path="functions" element={<Functions />} />
+        <Route path="hosting" element={<Hosting />} />
+        <Route path="machine-learning" element={<MachineLearning />} />
+        <Route path="storage" element={<Storage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
